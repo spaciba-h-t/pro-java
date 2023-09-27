050d81dae4c48151c26f9f0a46e201d9752c3d0b
spaciba-h-t <spaciba.h.t@gmail.com>
2023-09-28 05:59:02 +0900

    第3部9.1のサンプルプログラムをwhileに修正

--- part1/src/main/java/projava/ForSample.java
+++ part1/src/main/java/projava/ForSample.java
@@ -2,8 +2,10 @@ package projava;
 
 public class ForSample {
     public static void main(String[] args) {
-        for(int i=0; i<5; i++) {
+        int i =0;
+        while(i<5){
             System.out.println(i);
+            i++;
         }
     }
 }
