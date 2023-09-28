db47e39f03beece917e0bb2491f4c311a0350a7a
spaciba-h-t <spaciba.h.t@gmail.com>
2023-09-29 05:26:37 +0900

    for内部の処理を一部改修

--- part1/src/main/java/projava/ForSample.java
+++ part1/src/main/java/projava/ForSample.java
@@ -2,10 +2,13 @@ package projava;
 
 public class ForSample {
     public static void main(String[] args) {
-        int i =0;
-        while(i<5){
-            System.out.println(i);
-            i++;
+        for(int i=0; i<5;i++){
+            if(i==2) {
+                System.out.println("finish");
+                break;
+            }else {
+                System.out.println(i);
+            }
         }
     }
 }
