66d6a363d5d0d07c7f45d5a383a34e211aaeaa31
spaciba-h-t <spaciba.h.t@gmail.com>
2023-10-09 00:16:12 +0900

    第3部10.1.4のサンプルプログラム2を作成

---
+++ part1/src/main/java/projava/StreamSample2.java
@@ -0,0 +1,17 @@
+package projava;
+
+import java.util.List;
+
+public class StreamSample2 {
+    public static void main(String[] args) {
+        var data = List.of("yamamoto", "kis", "sugiyama");
+
+        var result = 0;
+        for (String s : data) {
+            if(s.length() >= 5) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
