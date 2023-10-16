bbe17492c07a0ed2ab41946463bea6f98c26817e
spaciba-h-t <spaciba.h.t@gmail.com>
2023-10-17 06:03:04 +0900

    第4部14.2.1のサンプルプログラムを書き換え

---
+++ part1/src/main/java/projava/InterfaceSample.java
@@ -0,0 +1,18 @@
+package projava;
+
+import java.util.List;
+
+public class InterfaceSample {
+    record Student(String name, int score) {}
+    record Teacher(String name, String subject) {}
+
+    public static void main(String[] args) {
+        var people = List.of(new Student("kis",80), new Teacher("hosoya", "Math"));
+        for (var p : people) {
+            var n = p instanceof Student s ? s.name() :
+                    p instanceof Teacher t ? t.name() :
+                            "---";
+            System.out.println("こんにちは%sさん".formatted(n));
+        }
+    }
+}
