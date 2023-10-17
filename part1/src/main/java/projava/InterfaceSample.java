b13c62a6ae17e198e986854cbe22661150467b83
spaciba-h-t <spaciba.h.t@gmail.com>
2023-10-18 05:28:19 +0900

    第4部14.2.2サンプルプログラムにメソッド追加

--- part1/src/main/java/projava/InterfaceSample.java
+++ part1/src/main/java/projava/InterfaceSample.java
@@ -10,8 +10,15 @@ public class InterfaceSample {
     record Student(String name, int score) implements Named {}
     record Teacher(String name, String subject) implements Named{}
 
+    static class Passenger implements Named {
+
+        @Override
+        public String name() {
+            return "通りすがり";
+        }
+    }
     public static void main(String[] args) {
-        var people = List.of(new Student("kis",80), new Teacher("hosoya", "Math"));
+        var people = List.of(new Student("kis",80), new Teacher("hosoya", "Math"), new Passenger());
         for (Named p : people) {
             var n = p.name();
             System.out.println("こんにちは%sさん".formatted(n));
