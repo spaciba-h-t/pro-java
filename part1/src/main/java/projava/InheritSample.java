c350cd0d58df002a94a61da585738a7ff2711e0a
spaciba-h-t <spaciba.h.t@gmail.com>
2023-10-18 05:49:24 +0900

    第4部15.1.1のサンプルプログラムを作成

---
+++ part1/src/main/java/projava/InheritSample.java
@@ -0,0 +1,48 @@
+package projava;
+
+import javax.security.auth.Subject;
+import java.util.List;
+
+public class InheritSample {
+    static class User {
+        String name;
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    static class Student extends User {
+        int score;
+
+        Student(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        public int getScore(){
+            return score;
+        }
+    }
+
+    static class Teacher extends User {
+        String subject;
+
+        Teacher(String name, String subject) {
+            this.name = name;
+            this.subject = subject;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<User> people = List.of(new Student("kis", 80),
+                new Teacher("hosoya", "Math"));
+        for (var p : people) {
+            System.out.println("こんにちは%sさん".formatted(p.getName()));
+        }
+    }
+}
